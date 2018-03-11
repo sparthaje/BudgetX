d87a3afc6a3d610246e6bd7207ced432f1658143
= <shreepa.parthaje@gmail.com>
2018-03-10 22:21:42 -0500

    Login Works

--- BudgetX/settings.py
+++ BudgetX/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login.apps.LoginConfig',
+    'dashboard.apps.DashboardConfig',
+    'upload.apps.UploadConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
