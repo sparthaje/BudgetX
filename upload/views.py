8b2838949d5bb6f6ae0a5750450c821e0f68be28
AnkitKhandelwal11 <36136482+AnkitKhandelwal11@users.noreply.github.com>
2018-03-11 04:04:24 -0400

    FUCK IT

--- upload/views.py
+++ upload/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from dashboard.models import BudgetCategory, SubBudgets
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'upload/index.html')
@@ -11,5 +12,4 @@ def budgethandler(request):
     amount=request.POST["amount"]
     cat = user.budgetcategory_set.create(name=category, total_cost=amount)
     cat.save()
-    context = {"name" : category}
-    return render(request, 'upload/subcategory.html', context)
+    return render(request, 'upload/subcategory.html', {'category' : category})
