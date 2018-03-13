55ffd502da4cdfe2eda48d501b80184b489b37e2
= <shreepa.parthaje@gmail.com>
2018-03-12 20:31:40 -0400

    random colors

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import BudgetCategory, SubBudgets
+from random import randint
 
 # Create your views here.
 def dash(request):
@@ -12,9 +13,20 @@ def dash(request):
 def redirect(request):
     bname = request.POST["budget_name"]
     bcategory = BudgetCategory.objects.get(name__startswith = bname)
+    colors = []
     sbudgets = bcategory.subbudgets_set.all()
+    
+    for i in range(len(sbudgets)):
+        r, g, b = randint(0,255), randint(0, 255), randint(0, 255)
+        s = "rgb({0}, {1}, {2})".format(r, g, b)
+        print(s)
+        colors.append(s)
+    
+    print(colors)
     context = {
         "bcategory": bcategory,
-        "sbudgets": sbudgets,
+        "sbudgets": [(sbudgets[s], colors[s]) for s in range(len(sbudgets))],
+        "r": len(sbudgets),
     }
+    print(context['sbudgets'])
     return render(request, 'dashboard/pi.html', context)
