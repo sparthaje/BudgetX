ca19934f2941d780fdb57e9c7caeee86b1a45862
= <shreepa.parthaje@gmail.com>
2018-03-12 20:08:14 -0400

    Merge branch 'master' of https://github.com/sparthaje/BudgetX

--- upload/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'upload'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('budgethandler/', views.budgethandler, name = 'budgethandler'),
-    path('sub_handler/', views.sub_handler, name = 'sub_handler'),
-]
